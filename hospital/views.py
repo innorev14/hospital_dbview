bdc6cfc0c1649eeccffb5060e2541197eb091bf1
innorev14 <tokyo3@netsgo.com>
2020-04-03 10:49:12 +0900

    fix/ remove using(database) in query

--- hospital/views.py
+++ hospital/views.py
@@ -12,24 +12,24 @@ class KeywordView(ListView):
 def keywordview(request):
     if request.method == "GET":
         req_keyword = request.GET.get('q', '')
-        dept = Department.objects.using('read1').all()
+        dept = Department.objects.all()
 
         req_dept = request.GET.get('dept', '')
-        dept_keyword = DeptAndKeyword.objects.using('read1').filter(Q(dept_id=req_dept)).order_by('name')
+        dept_keyword = DeptAndKeyword.objects.filter(Q(dept_id=req_dept)).order_by('name')
 
         req_depth1 = request.GET.get('depth1', '')
-        depth_one = DepthOneKeyword.objects.using('read1').all()
-        depth1_rel = DepthOneRel.objects.using('read2').filter(Q(keyword=req_depth1)).order_by('relkeyword')
+        depth_one = DepthOneKeyword.objects.all()
+        depth1_rel = DepthOneRel.objects.filter(Q(keyword=req_depth1)).order_by('relkeyword')
         req_depth2 = request.GET.get('depth2', '')
-        depth_two = DepthTwoKeyword.objects.using('read1').all()
-        depth2_rel= DepthTwoRel.objects.using('read2').filter(Q(keyword=req_depth2)).order_by('relkeyword')
+        depth_two = DepthTwoKeyword.objects.all()
+        depth2_rel= DepthTwoRel.objects.filter(Q(keyword=req_depth2)).order_by('relkeyword')
         req_depth3 = request.GET.get('depth3', '')
-        depth_three = DepthThreeKeyword.objects.using('read1').all()
-        depth3_rel= DepthThreeRel.objects.using('read2').filter(Q(keyword=req_depth3)).order_by('relkeyword')
+        depth_three = DepthThreeKeyword.objects.all()
+        depth3_rel= DepthThreeRel.objects.filter(Q(keyword=req_depth3)).order_by('relkeyword')
 
 
-        rel_keyword = JamesMainKeywordRel.objects.using('read1').filter(Q(main_keyword=req_keyword))
-        naverapi = JamesKeywordNaverapi.objects.using('read2').filter(Q(keyword=req_keyword))
+        rel_keyword = JamesMainKeywordRel.objects.filter(Q(main_keyword=req_keyword))
+        naverapi = JamesKeywordNaverapi.objects.filter(Q(keyword=req_keyword))
 
         context = {
             'dept': dept,
@@ -48,11 +48,11 @@ def keywordview(request):
     if request.method == 'POST':
         req_keyword = request.GET.get('q', '')
         print(req_keyword)
-        dept = Department.objects.using('read1').values()
-        dept_keyword = DeptAndKeyword.objects.using('read1').select_related().order_by('name')
+        dept = Department.objects.values()
+        dept_keyword = DeptAndKeyword.objects.select_related().order_by('name')
 
-        rel_keyword = JamesMainKeywordRel.objects.using('read1').values()
-        naverapi = JamesKeywordNaverapi.objects.using('read2').values()
+        rel_keyword = JamesMainKeywordRel.objects.values()
+        naverapi = JamesKeywordNaverapi.objects.values()
 
         if req_keyword in dept_keyword.select_related('dept_id'):
             context = {
@@ -73,7 +73,7 @@ def keywordview(request):
 
 
 def naverAPIListView(request):
-    naverapi = JamesKeywordNaverapi.objects.using('read2').values()
+    naverapi = JamesKeywordNaverapi.objects.values()
     context = {
         'naverapi': naverapi,
     }

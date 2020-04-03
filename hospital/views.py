a3f6802e26a8cc3bf6d3e29b0b78ee5630a62486
innorev14 <tokyo3@netsgo.com>
2020-04-03 11:07:19 +0900

    feat/ add db counter

--- hospital/views.py
+++ hospital/views.py
@@ -16,32 +16,44 @@ def keywordview(request):
 
         req_dept = request.GET.get('dept', '')
         dept_keyword = DeptAndKeyword.objects.filter(Q(dept_id=req_dept)).order_by('name')
+        dept_count = DeptAndKeyword.objects.count()
 
         req_depth1 = request.GET.get('depth1', '')
         depth_one = DepthOneKeyword.objects.all()
         depth1_rel = DepthOneRel.objects.filter(Q(keyword=req_depth1)).order_by('relkeyword')
+        depth1_count = DepthOneRel.objects.count()
+
         req_depth2 = request.GET.get('depth2', '')
         depth_two = DepthTwoKeyword.objects.all()
         depth2_rel= DepthTwoRel.objects.filter(Q(keyword=req_depth2)).order_by('relkeyword')
+        depth2_count = DepthTwoRel.objects.count()
+
         req_depth3 = request.GET.get('depth3', '')
         depth_three = DepthThreeKeyword.objects.all()
         depth3_rel= DepthThreeRel.objects.filter(Q(keyword=req_depth3)).order_by('relkeyword')
+        depth3_count = DepthThreeRel.objects.count()
 
 
         rel_keyword = JamesMainKeywordRel.objects.filter(Q(main_keyword=req_keyword))
-        naverapi = JamesKeywordNaverapi.objects.filter(Q(keyword=req_keyword))
+        naverapi = JamesKeywordNaverapi.objects.filter(Q(relkeyword=req_keyword)).order_by('-mobileclick')
+        rel_keyword_count = JamesKeywordNaverapi.objects.count()
 
         context = {
             'dept': dept,
             'dept_keyword': dept_keyword,
+            'dept_count': dept_count,
             'rel_keyword': rel_keyword,
             'naverapi': naverapi,
+            'rel_keyword_count': rel_keyword_count,
             'depth_one': depth_one,
             'depth1_rel': depth1_rel,
+            'depth1_count': depth1_count,
             'depth_two': depth_two,
             'depth2_rel': depth2_rel,
+            'depth2_count': depth2_count,
             'depth_three': depth_three,
             'depth3_rel': depth3_rel,
+            'depth3_count': depth3_count,
         }
         return render(request, template_name='hospital/department_list.html', context=context)
 
